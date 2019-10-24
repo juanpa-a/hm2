24d7f9c2ba2c36d03b062ae6872b89de86d6e3f0
Juan Pablo Ascencio <jpabloascencioglez@gmail.com>
2019-10-24 10:44:54 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,4 @@
-var firebaseConfig = {
-    authDomain: "gif-chat-a251e.firebaseapp.com",
-    databaseURL: "https://gif-chat-a251e.firebaseio.com",
-    projectId: "gif-chat-a251e",
-    storageBucket: "gif-chat-a251e.appspot.com",
-    messagingSenderId: "870690375347",
-    appId: "1:870690375347:web:03663e9aa6b0b72abb12c6"
-};
+
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
